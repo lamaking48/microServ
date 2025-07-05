6c48d2159ad35efaaddf3c16315ddf8e371b9da8
Ivan Bro <lama.king.2000@mail.ru>
2025-07-06 00:46:31 +0300

    second try

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	handlers "_/D_/MicroServ"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/lamaking48/microServ/tree/main/handlers"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello()
+	hh := handlers.NewHello(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
