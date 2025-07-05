a6b5d54551f0ae4b87c084ec1fa1e9204bde5b41
Ivan Bro <lama.king.2000@mail.ru>
2025-07-06 00:38:59 +0300

    try

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	handlers "_/D_/MicroServ"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello()
 
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 	http.ListenAndServe(":9090", nil)
 }
